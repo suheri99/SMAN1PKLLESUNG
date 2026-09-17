e7f82cfc2f407c67732af98918443396a1c0c9bb
suheri99 <147498373+suheri99@users.noreply.github.com>
2026-09-18 06:02:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ document.body.style.overflow = 'hidden';
 // TYPING EFFECT
 // ============================================
 const typingText = document.getElementById('typingText');
-const words = ['Siswa', 'Premium', 'Mewah', 'Royal'];
+const words = ['Siswa', 'SMAN1', 'Pangkalan', 'Lesung'];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
